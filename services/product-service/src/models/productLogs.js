842ddaaa0e593b198f4ccf54d2b1c665f2ab77b1
Flutterneu <rsmx141@gmail.com>
2025-07-16 13:55:13 +0530

    feat: implement user creation logic with Firebase token verification; add error handling for existing users

--- services/product-service/src/models/productLogs.js
+++ services/product-service/src/models/productLogs.js
@@ -1,54 +1,54 @@
-const { JsonWebTokenError } = require("jsonwebtoken");
-const mongoose = require("mongoose");
+  const { JsonWebTokenError } = require("jsonwebtoken");
+  const mongoose = require("mongoose");
 
-const productLogSchema = new mongoose.Schema({
-  job_type: {
-    type: String,
-    enum: [
-      "Update",
-      "Edit",
-      "Added",
-      "Bulk-Upload",
-      "Single-Upload",
-      "Bulk-Modified",
-      "Stock-Sweep",
-    ],
+  const productLogSchema = new mongoose.Schema({
+    job_type: {
+      type: String,
+      enum: [
+        "Update",
+        "Edit",
+        "Added",
+        "Bulk-Upload",
+        "Single-Upload",
+        "Bulk-Modified",
+        "Stock-Sweep",
+      ],
 
-    required: true,
-  },
-  product_ref: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
-  user: {
-    type: String,
-  },
-  updated_at: {
-    type: Date,
-    default: Date.now,
-  },
-  changed_fields: {
-    type: Array,
-    default: [],
-  },
-  changed_value: [
-    {
-      field: {
-        type: String,
-      },
-      old_value: {
-        type: String,
-      },
-      new_value: {
-        type: String,
-      },
+      required: true,
     },
-  ],
-  meta: { type: mongoose.Schema.Types.Mixed },
-});
+    product_ref: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: String,
+    },
+    updated_at: {
+      type: Date,
+      default: Date.now,
+    },
+    changed_fields: {
+      type: Array,
+      default: [],
+    },
+    changed_value: [
+      {
+        field: {
+          type: String,
+        },
+        old_value: {
+          type: String,
+        },
+        new_value: {
+          type: String,
+        },
+      },
+    ],
+    meta: { type: mongoose.Schema.Types.Mixed },
+  });
 
-module.exports = mongoose.model("ProductLogs", productLogSchema);
+  module.exports = mongoose.model("ProductLogs", productLogSchema);
