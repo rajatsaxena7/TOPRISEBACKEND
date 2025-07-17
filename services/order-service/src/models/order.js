50d6d0488b767654097461011744817a2ad5ac86
Flutterneu <rsmx141@gmail.com>
2025-07-17 16:08:02 +0530

    fix: Add missing order_Amount field to OrderSchema

--- services/order-service/src/models/order.js
+++ services/order-service/src/models/order.js
@@ -14,6 +14,7 @@ const OrderSchema = new mongoose.Schema(
         productName: String,
       },
     ],
+    order_Amount: Number,
     customerDetails: {
       userId: String,
       name: String,
