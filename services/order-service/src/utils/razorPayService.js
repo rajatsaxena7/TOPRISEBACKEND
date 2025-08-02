76a0cd618e305f9c5f58db55db0154de7a62872a
jayanthbr29 <jayanthbr@digi9.co.in>
2025-08-02 12:58:09 +0530

    payment webhook tested

--- services/order-service/src/utils/razorPayService.js
+++ services/order-service/src/utils/razorPayService.js
@@ -1,8 +1,8 @@
 const Razorpay = require('razorpay')
 const razorpayInstance = new Razorpay({
-    key_id: process.env.RAZORPAYKEY_ID_DEV||"rzp_test_JukdKxCGZqamy4",//developmet
+    key_id: process.env.RAZORPAYKEY_ID_DEV||"rzp_test_je9PPHh0HQqIFX",//developmet
     // key_id:'rzp_live_nmEtTvImeXIvf7',//production
-    key_secret: process.env.RAZORPAYKEY_SECRET_DEV||"Pafcggj10dqbLCnZxqCyTojt", //developmet
+    key_secret: process.env.RAZORPAYKEY_SECRET_DEV||"tdB9zkAvQLdYVRKgtOWNfIhZ", //developmet
     // key_secret:'u4QbAJziwXhGYmdFWamz2KkZ',//production
 })
 
