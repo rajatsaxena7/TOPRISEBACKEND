720b10168530a5992df53c06488c72356ff6e55b
Flutterneu <rsmx141@gmail.com>
2025-10-12 18:15:59 +0530

    Update import path for audit middleware in SLA Violation Management routes

--- services/order-service/src/routes/slaViolationManagement.js
+++ services/order-service/src/routes/slaViolationManagement.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const SLAViolationManagementController = require("../controllers/slaViolationManagement");
 const { authenticate, authorizeRoles } = require("/packages/utils/authMiddleware");
-const auditMiddleware = require("../middleware/auditLogger");
+const auditMiddleware = require("../utils/auditLogger");
 
 /**
  * @route POST /api/sla-violations/manual
